5b12e50d3b5029bfc2ad71ffafa08d6bc506d786
Manon Brousse <pandillamanolito@gmail.com>
2023-01-30 20:19:14 -0600

    modal and work layout

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 import '../scss/main.scss'
-import 'animate.css'
 
-// if (
-//     "IntersectionObserver" in window &&
-//     "IntersectionObserverEntry" in window &&
-//     "intersectionRatio" in window.IntersectionObserverEntry.prototype
-// ) {
-//     let observer = new IntersectionObserver(entries => {
-//         if (entries[0].boundingClientRect.y < 0) {
-//             document.body.classList.add("header-not-at-top");
-//         } else {
-//             document.body.classList.remove("header-not-at-top");
-//         }
-//     });
-//     observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
-// }
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".btn-close");
+const workClose = document.querySelector(".work-close");
+const workModal = document.querySelector("#aramidModal")
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    document.body.classList.remove("no-scroll");
+
+};
+
+const closeWorkModal = function () {
+    workModal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    document.body.classList.remove("no-scroll");
+
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
+
+document.addEventListener('click', function (event) {
+
+    if (event.target.matches('.btn-open')) {
+
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+        document.body.classList.add("no-scroll");
+    }
+
+});
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", closeModal);
